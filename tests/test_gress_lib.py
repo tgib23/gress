2e7d345363074b7919888200a01e091b9be76773
Satoshi Yamada <bigt2323@gmail.com>
2021-02-08 23:39:07 +0900

    add increment command test

--- tests/test_gress_lib.py
+++ tests/test_gress_lib.py
@@ -25,14 +25,33 @@ class Test_gress_lib(unittest.TestCase):
 
     def test_decrement_command(self):
         with mock.patch('modules.gress_lib.Gress.display_lines'):
-            obj = Gress('test', 'tests/testf')
-            obj.decrement_command('k')
-            self.assertEqual(obj.grep_highlight_index, 0)
-            self.assertEqual(obj.grep_index, 0)
-            obj.grep_highlight_index = 10
-            obj.decrement_command('k')
-            self.assertEqual(obj.grep_highlight_index, 9)
-            self.assertEqual(obj.grep_index, 0)
+            obj_grep_k = Gress('test', 'tests/testf')
+            obj_grep_k.decrement_command('k')
+            self.assertEqual(obj_grep_k.grep_highlight_index, 0)
+            self.assertEqual(obj_grep_k.grep_index, 0)
+            obj_grep_k.grep_highlight_index = 10
+            obj_grep_k.decrement_command('k')
+            self.assertEqual(obj_grep_k.grep_highlight_index, 9)
+            self.assertEqual(obj_grep_k.grep_index, 0)
+
+            obj_file_k = Gress('test', 'tests/testf')
+            obj_file_k.mode = 'file'
+            obj_file_k.file_index = 200
+            obj_file_k.decrement_command('k')
+            self.assertEqual(obj_file_k.file_index, 199)
+
+    def test_increment_command(self):
+        with mock.patch('modules.gress_lib.Gress.display_lines'):
+            obj_grep_j = Gress('test', 'tests/testf')
+            obj_grep_j.increment_command('j')
+            self.assertEqual(obj_grep_j.grep_highlight_index, 1)
+            self.assertEqual(obj_grep_j.grep_index, 1)
+
+            obj_file_j = Gress('test', 'tests/testf')
+            obj_file_j.mode = 'file'
+            obj_file_j.file_index = 20
+            obj_file_j.increment_command('j')
+            self.assertEqual(obj_file_j.file_index, 21)
 
 if __name__ == "__main__":
     unittest.main()
